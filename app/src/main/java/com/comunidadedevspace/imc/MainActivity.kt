c4da360306fd7295b010ad96e68c7abfc3d368a3
Emidio Valeretto <emidio.valereto@gmail.com>
2024-06-06 15:00:51 +0100

    chore: retrieve weight and height values

--- app/src/main/java/com/comunidadedevspace/imc/MainActivity.kt
+++ app/src/main/java/com/comunidadedevspace/imc/MainActivity.kt
@@ -2,10 +2,24 @@ package com.comunidadedevspace.imc
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import com.google.android.material.textfield.TextInputEditText
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val weightUserInput = findViewById<TextInputEditText>(R.id.weight_user_input)
+        val heightUserInput = findViewById<TextInputEditText>(R.id.height_user_input)
+
+        val btnCalculate = findViewById<Button>(R.id.btn_calculate)
+
+        btnCalculate.setOnClickListener {
+            val weight = weightUserInput.text
+            val height  = heightUserInput.text
+
+            println("Weight: $weight | Height: $height")
+        }
     }
 }
